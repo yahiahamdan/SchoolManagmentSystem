7a66ca167f04d549fe17fb3db45f9b60068f6d18
yahia hamdan <hamdanyahia2@gmail.com>
2024-04-07 00:09:40 +0200

    adding ApplicationDBContext in the infrastucture and installing the  core-tools-sqlserver

---
+++ Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+
+        }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+: base(options)
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Subject> Subjects { get; set; }
+        public DbSet<StudentSubject> StudentSubjects { get; set; }
+        public DbSet<DepartmentSubject> DepartmentSubjects { get; set; }
+
+
+
+    }
+   
+}
