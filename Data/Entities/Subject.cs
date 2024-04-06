e4e940281eb9442e9aeb1fe7fd76b3e571b4428c
yahia hamdan <hamdanyahia2@gmail.com>
2024-04-07 00:14:57 +0200

    updating dataentities

--- Data/Entities/Subject.cs
+++ Data/Entities/Subject.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Data.Entities
 {
-    internal class Subject
+    public class Subject
     {
+        public Subject()
+        {
+            StudentsSubjects = new HashSet<StudentSubject>();
+            DepartmetsSubjects = new HashSet<DepartmentSubject>();
+        }
+
+
+        [Key]
+        public int SubID { get; set; }
+        [StringLength(500)]
+        public string SubjectName { get; set; }
+        public DateTime Period { get; set; }
+        public virtual ICollection<StudentSubject> StudentsSubjects { get; set; }
+        public virtual ICollection<DepartmentSubject> DepartmetsSubjects { get; set; }
+
     }
 }
